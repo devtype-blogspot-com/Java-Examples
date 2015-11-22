1c76c0037287733749bdc7d760ed96a221dd03e4
DEVTYPE <Informatik.on.GitHub@gmail.com>
2015-11-23 04:22:27 +0600

    Prefix-free code

---
+++ PrefixFreeCode/src/Main.java
@@ -0,0 +1,378 @@
+import java.io.*;
+import java.util.*;
+
+class MinPQ<Key> implements Iterable<Key> {
+    private Key[] pq;                    // store items at indices 1 to N
+    private int N;                       // number of items on priority queue
+    private Comparator<Key> comparator;  // optional comparator
+
+    /**
+     * Initializes an empty priority queue with the given initial capacity.
+     *
+     * @param  initCapacity the initial capacity of this priority queue
+     */
+    public MinPQ(int initCapacity) {
+        pq = (Key[]) new Object[initCapacity + 1];
+        N = 0;
+    }
+
+    /**
+     * Initializes an empty priority queue.
+     */
+    public MinPQ() {
+        this(1);
+    }
+
+    /**
+     * Initializes an empty priority queue with the given initial capacity,
+     * using the given comparator.
+     *
+     * @param  initCapacity the initial capacity of this priority queue
+     * @param  comparator the order to use when comparing keys
+     */
+    public MinPQ(int initCapacity, Comparator<Key> comparator) {
+        this.comparator = comparator;
+        pq = (Key[]) new Object[initCapacity + 1];
+        N = 0;
+    }
+
+    /**
+     * Initializes an empty priority queue using the given comparator.
+     *
+     * @param  comparator the order to use when comparing keys
+     */
+    public MinPQ(Comparator<Key> comparator) {
+        this(1, comparator);
+    }
+
+    /**
+     * Initializes a priority queue from the array of keys.
+     * <p>
+     * Takes time proportional to the number of keys, using sink-based heap construction.
+     *
+     * @param  keys the array of keys
+     */
+    public MinPQ(Key[] keys) {
+        N = keys.length;
+        pq = (Key[]) new Object[keys.length + 1];
+        for (int i = 0; i < N; i++)
+            pq[i+1] = keys[i];
+        for (int k = N/2; k >= 1; k--)
+            sink(k);
+        assert isMinHeap();
+    }
+
+    /**
+     * Returns true if this priority queue is empty.
+     *
+     * @return <tt>true</tt> if this priority queue is empty;
+     *         <tt>false</tt> otherwise
+     */
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    /**
+     * Returns the number of keys on this priority queue.
+     *
+     * @return the number of keys on this priority queue
+     */
+    public int size() {
+        return N;
+    }
+
+    /**
+     * Returns a smallest key on this priority queue.
+     *
+     * @return a smallest key on this priority queue
+     * @throws NoSuchElementException if this priority queue is empty
+     */
+    public Key min() {
+        if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
+        return pq[1];
+    }
+
+    // helper function to double the size of the heap array
+    private void resize(int capacity) {
+        assert capacity > N;
+        Key[] temp = (Key[]) new Object[capacity];
+        for (int i = 1; i <= N; i++) {
+            temp[i] = pq[i];
+        }
+        pq = temp;
+    }
+
+    /**
+     * Adds a new key to this priority queue.
+     *
+     * @param  x the key to add to this priority queue
+     */
+    public void insert(Key x) {
+        // double size of array if necessary
+        if (N == pq.length - 1) resize(2 * pq.length);
+
+        // add x, and percolate it up to maintain heap invariant
+        pq[++N] = x;
+        swim(N);
+        assert isMinHeap();
+    }
+
+    /**
+     * Removes and returns a smallest key on this priority queue.
+     *
+     * @return a smallest key on this priority queue
+     * @throws NoSuchElementException if this priority queue is empty
+     */
+    public Key delMin() {
+        if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
+        exch(1, N);
+        Key min = pq[N--];
+        sink(1);
+        pq[N+1] = null;         // avoid loitering and help with garbage collection
+        if ((N > 0) && (N == (pq.length - 1) / 4)) resize(pq.length  / 2);
+        assert isMinHeap();
+        return min;
+    }
+
+
+    /***************************************************************************
+     * Helper functions to restore the heap invariant.
+     ***************************************************************************/
+
+    private void swim(int k) {
+        while (k > 1 && greater(k/2, k)) {
+            exch(k, k/2);
+            k = k/2;
+        }
+    }
+
+    private void sink(int k) {
+        while (2*k <= N) {
+            int j = 2*k;
+            if (j < N && greater(j, j+1)) j++;
+            if (!greater(k, j)) break;
+            exch(k, j);
+            k = j;
+        }
+    }
+
+    /***************************************************************************
+     * Helper functions for compares and swaps.
+     ***************************************************************************/
+    private boolean greater(int i, int j) {
+        if (comparator == null) {
+            return ((Comparable<Key>) pq[i]).compareTo(pq[j]) > 0;
+        }
+        else {
+            return comparator.compare(pq[i], pq[j]) > 0;
+        }
+    }
+
+    private void exch(int i, int j) {
+        Key swap = pq[i];
+        pq[i] = pq[j];
+        pq[j] = swap;
+    }
+
+    // is pq[1..N] a min heap?
+    private boolean isMinHeap() {
+        return isMinHeap(1);
+    }
+
+    // is subtree of pq[1..N] rooted at k a min heap?
+    private boolean isMinHeap(int k) {
+        if (k > N) return true;
+        int left = 2*k, right = 2*k + 1;
+        if (left  <= N && greater(k, left))  return false;
+        if (right <= N && greater(k, right)) return false;
+        return isMinHeap(left) && isMinHeap(right);
+    }
+
+
+    /**
+     * Returns an iterator that iterates over the keys on this priority queue
+     * in ascending order.
+     * <p>
+     * The iterator doesn't implement <tt>remove()</tt> since it's optional.
+     *
+     * @return an iterator that iterates over the keys in ascending order
+     */
+    public Iterator<Key> iterator() { return new HeapIterator(); }
+
+    private class HeapIterator implements Iterator<Key> {
+        // create a new pq
+        private MinPQ<Key> copy;
+
+        // add all items to copy of heap
+        // takes linear time since already in heap order so no keys move
+        public HeapIterator() {
+            if (comparator == null) copy = new MinPQ<Key>(size());
+            else                    copy = new MinPQ<Key>(size(), comparator);
+            for (int i = 1; i <= N; i++)
+                copy.insert(pq[i]);
+        }
+
+        public boolean hasNext()  { return !copy.isEmpty();                     }
+        public void remove()      { throw new UnsupportedOperationException();  }
+
+        public Key next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            return copy.delMin();
+        }
+    }
+}
+
+
+public class Main {
+    // alphabet size of extended ASCII
+    private static final int R = 256;
+
+    // Do not instantiate.
+    private Main() { }
+
+    // Huffman trie node
+    private static class Node implements Comparable<Node> {
+        private final char ch;
+        private final int freq;
+        private final Node left, right;
+
+        Node(char ch, int freq, Node left, Node right) {
+            this.ch    = ch;
+            this.freq  = freq;
+            this.left  = left;
+            this.right = right;
+        }
+
+        // is the node a leaf node?
+        private boolean isLeaf() {
+            assert ((left == null) && (right == null)) || ((left != null) && (right != null));
+            return (left == null) && (right == null);
+        }
+
+        // compare, based on frequency
+        public int compareTo(Node that) {
+            return this.freq - that.freq;
+        }
+    }
+
+    // build the Huffman trie given frequencies
+    private static Node buildTrie(int[] freq) {
+
+        // initialze priority queue with singleton trees
+        MinPQ<Node> pq = new MinPQ<Node>();
+        for (char i = 0; i < R; i++)
+            if (freq[i] > 0)
+                pq.insert(new Node(i, freq[i], null, null));
+
+        // special case in case there is only one character with a nonzero frequency
+        if (pq.size() == 1) {
+            if (freq['\0'] == 0) pq.insert(new Node('\0', 0, null, null));
+            else                 pq.insert(new Node('\1', 0, null, null));
+        }
+
+        // merge two smallest trees
+        while (pq.size() > 1) {
+            Node left  = pq.delMin();
+            Node right = pq.delMin();
+            Node parent = new Node('\0', left.freq + right.freq, left, right);
+            pq.insert(parent);
+        }
+        return pq.delMin();
+    }
+
+    // make a lookup table from symbols and their encodings
+    private static void buildCode(String[] st, Node x, String s) {
+        if (!x.isLeaf()) {
+            buildCode(st, x.left,  s + '0');
+            buildCode(st, x.right, s + '1');
+        }
+        else {
+            st[x.ch] = s;
+        }
+    }
+
+    /**
+     * Reads a sequence of 8-bit bytes from standard input; compresses them
+     * using Huffman codes with an 8-bit alphabet; and writes the results
+     * to standard output.
+     */
+    public static void main(String[] args) {
+        // read the input
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        char[] input = s.toCharArray();
+
+        // tabulate frequency counts
+        int[] freq = new int[R];
+        int k = 0;
+        for (int i = 0; i < input.length; i++) {
+            freq[input[i]]++;
+            if (freq[input[i]] == 1) k++;
+        }
+
+        // build Huffman trie
+        Node root = buildTrie(freq);
+
+        // build code table
+        String[] st = new String[R];
+        buildCode(st, root, "");
+
+        // use Huffman code to encode input
+        StringBuilder encodedInput = new StringBuilder();
+        for (int i = 0; i < input.length; i++) {
+            String code = st[input[i]];
+            for (int j = 0; j < code.length(); j++) {
+                encodedInput.append(code.charAt(j));
+            }
+        }
+
+        StringBuilder letterCodes = new StringBuilder();
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] != 0) {
+                String code = st[i];
+                letterCodes.append(((char) (i)) + ": " + code + "\n");
+            }
+        }
+
+        System.out.println(k + " " + encodedInput.length());
+        System.out.println(letterCodes.toString());
+        System.out.print(encodedInput.toString());
+    }
+}
+
+/*
+
+По данной непустой строке s длины не более 10^4, состоящей из строчных букв латинского алфавита, постройте оптимальный беспрефиксный код.
+В первой строке выведите количество различных букв k, встречающихся в строке, и размер получившейся закодированной строки.
+В следующих k строках запишите коды букв в формате "letter: code". В последней строке выведите закодированную строку.
+
+
+Sample Input 1:
+a
+
+Sample Output 1:
+1 1
+a: 0
+0
+
+
+Sample Input 2:
+abacabad
+
+Sample Output 2:
+4 14
+a: 0
+b: 10
+c: 110
+d: 111
+01001100100111
+
+ */
+
+
+/*
+http://algs4.cs.princeton.edu/55compression/
+http://algs4.cs.princeton.edu/55compression/Huffman.java.html
+http://algs4.cs.princeton.edu/24pq/MinPQ.java.html
+http://algs4.cs.princeton.edu/55compression/BinaryStdOut.java.html
+ */
