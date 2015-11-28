5c42440455c293d0cfdbd62dbec364ce2d5cad42
DEVTYPE <Informatik.on.GitHub@gmail.com>
2015-11-29 03:13:24 +0600

    Count inversions in array

---
+++ CountInversionsInArray/src/Main.java
@@ -0,0 +1,165 @@
+import java.util.Scanner;
+
+/**
+ * @see http://algs4.cs.princeton.edu/22mergesort/Inversions.java.html
+ */
+public class Main {
+
+    // do not instantiate
+    private Main() { }
+
+    // merge and count
+    private static long merge(int[] a, int[] aux, int lo, int mid, int hi) {
+        long inversions = 0;
+
+        // copy to aux[]
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        // merge back to a[]
+        int i = lo, j = mid+1;
+        for (int k = lo; k <= hi; k++) {
+            if      (i > mid)           a[k] = aux[j++];
+            else if (j > hi)            a[k] = aux[i++];
+            else if (aux[j] < aux[i]) { a[k] = aux[j++]; inversions += (mid - i + 1); }
+            else                        a[k] = aux[i++];
+        }
+        return inversions;
+    }
+
+    // return the number of inversions in the subarray b[lo..hi]
+    // side effect b[lo..hi] is rearranged in ascending order
+    private static long count(int[] a, int[] b, int[] aux, int lo, int hi) {
+        long inversions = 0;
+        if (hi <= lo) return 0;
+        int mid = lo + (hi - lo) / 2;
+        inversions += count(a, b, aux, lo, mid);
+        inversions += count(a, b, aux, mid+1, hi);
+        inversions += merge(b, aux, lo, mid, hi);
+        assert inversions == brute(a, lo, hi);
+        return inversions;
+    }
+
+
+    /**
+     * Returns the number of inversions in the integer array.
+     * The argument array is not modified.
+     * @param  a the array
+     * @return the number of inversions in the array. An inversion is a pair of
+     *         indicies <tt>i</tt> and <tt>j</tt> such that <tt>i &lt; j</tt>
+     *         and <tt>a[i]</tt> &gt; <tt>a[j]</tt>.
+     */
+    public static long count(int[] a) {
+        int[] b   = new int[a.length];
+        int[] aux = new int[a.length];
+        for (int i = 0; i < a.length; i++)
+            b[i] = a[i];
+        long inversions = count(a, b, aux, 0, a.length - 1);
+        return inversions;
+    }
+
+
+
+    // merge and count (Comparable version)
+    private static long merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+        long inversions = 0;
+
+        // copy to aux[]
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        // merge back to a[]
+        int i = lo, j = mid+1;
+        for (int k = lo; k <= hi; k++) {
+            if      (i > mid)                a[k] = aux[j++];
+            else if (j > hi)                 a[k] = aux[i++];
+            else if (less(aux[j], aux[i])) { a[k] = aux[j++]; inversions += (mid - i + 1); }
+            else                             a[k] = aux[i++];
+        }
+        return inversions;
+    }
+
+    // return the number of inversions in the subarray b[lo..hi]
+    // side effect b[lo..hi] is rearranged in ascending order
+    private static long count(Comparable[] a, Comparable[] b, Comparable[] aux, int lo, int hi) {
+        long inversions = 0;
+        if (hi <= lo) return 0;
+        int mid = lo + (hi - lo) / 2;
+        inversions += count(a, b, aux, lo, mid);
+        inversions += count(a, b, aux, mid+1, hi);
+        inversions += merge(b, aux, lo, mid, hi);
+        assert inversions == brute(a, lo, hi);
+        return inversions;
+    }
+
+
+    /**
+     * Returns the number of inversions in the comparable array.
+     * The argument array is not modified.
+     * @param  a the array
+     * @return the number of inversions in the array. An inversion is a pair of
+     *         indicies <tt>i</tt> and <tt>j</tt> such that <tt>i &lt; j</tt>
+     *         and <tt>a[i].compareTo(a[j]) &gt; 0</tt>.
+     */
+    public static long count(Comparable[] a) {
+        Comparable[] b   = new Comparable[a.length];
+        Comparable[] aux = new Comparable[a.length];
+        for (int i = 0; i < a.length; i++)
+            b[i] = a[i];
+        long inversions = count(a, b, aux, 0, a.length - 1);
+        return inversions;
+    }
+
+
+    // is v < w ?
+    private static boolean less(Comparable v, Comparable w) {
+        return (v.compareTo(w) < 0);
+    }
+
+    // count number of inversions in a[lo..hi] via brute force (for debugging only)
+    private static long brute(Comparable[] a, int lo, int hi) {
+        long inversions = 0;
+        for (int i = lo; i <= hi; i++)
+            for (int j = i + 1; j <= hi; j++)
+                if (less(a[j], a[i])) inversions++;
+        return inversions;
+    }
+
+    // count number of inversions in a[lo..hi] via brute force (for debugging only)
+    private static long brute(int[] a, int lo, int hi) {
+        long inversions = 0;
+        for (int i = lo; i <= hi; i++)
+            for (int j = i + 1; j <= hi; j++)
+                if (a[j] < a[i]) inversions++;
+        return inversions;
+    }
+
+    /**
+     * Reads in a sequence of integers from standard input and prints
+     * the number of inversions.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        Integer[] b = new Integer[N];
+        for (int i = 0; i < N; i++)
+            b[i] = scanner.nextInt();
+        System.out.println(count(b));
+    }
+}
+
+/*
+ Задача на программирование: число инверсий
+
+ Первая строка содержит число 1≤n≤105, вторая — массив A[1…n], содержащий натуральные числа, не превосходящие 109. Необходимо посчитать число пар индексов 1≤i<j≤n, для которых A[i]>A[j]. (Такая пара элементов называется инверсией массива. Количество инверсий в массиве является в некотором смысле его мерой неупорядоченности: например, в упорядоченном по неубыванию массиве инверсий нет вообще, а в массиве, упорядоченном по убыванию, инверсию образуют каждые два элемента.)
+
+ Sample Input:
+ 5
+ 2 3 9 2 9
+
+ Sample Output:
+ 2
+
+*/
